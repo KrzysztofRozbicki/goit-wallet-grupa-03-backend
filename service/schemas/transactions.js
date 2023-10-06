400ab074e49ad9c3538e9f42be461e03116bf8e9
Łukasz Kocela <lukaszkocela@gmail.com>
2023-10-06 13:12:45 +0200

    1stv transactions get/del/upd/add

--- service/schemas/transactions.js
+++ service/schemas/transactions.js
@@ -2,38 +2,41 @@ import mongoose from 'mongoose';
 
 const { Schema } = mongoose;
 
-const transaction = new Schema({
-  user: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'user',
-  },
+const transaction = new Schema(
+  {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'user',
+    },
 
-  amount: {
-    type: Number,
-    required: true,
-  },
+    amount: {
+      type: Number,
+      required: true,
+    },
 
-  category: {
-    type: String,
-    required: true,
-  },
+    category: {
+      type: String,
+      required: true,
+    },
 
-  date: {
-    type: String,
-    required: true,
-  },
+    date: {
+      type: String,
+      required: true,
+    },
 
-  isIncome: {
-    type: Boolean,
-    required: true,
-    default: false,
-  },
+    isIncome: {
+      type: Boolean,
+      required: true,
+      default: false,
+    },
 
-  comment: {
-    type: String,
-    required: false,
+    comment: {
+      type: String,
+      required: false,
+    },
   },
-});
+  { versionKey: false, timestamps: true }
+);
 
 const Transaction = mongoose.model('transaction', transaction);
 
