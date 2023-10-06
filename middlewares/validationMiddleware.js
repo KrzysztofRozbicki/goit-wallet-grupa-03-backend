d80705b6e0dfb64fa0c2dcef971fa9697dec2726
MarcinNieradka <nieradka.marcin@gmail.com>
2023-10-06 10:40:49 +0200

    quick fixes

--- middlewares/validationMiddleware.js
+++ middlewares/validationMiddleware.js
@@ -1,13 +1,17 @@
 import Joi from 'joi';
 
 export const registrationSchema = Joi.object({
-  name: Joi.string().required(),
-  email: Joi.string().email().required(),
+  name: Joi.string().min(1).required(),
+  email: Joi.string()
+    .email({ tlds: { allow: false } })
+    .required(),
   password: Joi.string().min(1).required(),
 });
 
 export const loginSchema = Joi.object({
-  email: Joi.string().email().required(),
+  email: Joi.string()
+    .email({ tlds: { allow: false } })
+    .required(),
   password: Joi.string().required(),
 });
 
