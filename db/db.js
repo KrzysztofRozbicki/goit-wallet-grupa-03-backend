bfa5f9531175200c910bc494cf22a810230b0336
Krzysztof R <plkrozbicki@gmail.com>
2024-07-30 09:30:35 +0100

    Update for new backend service

--- db/db.js
+++ db/db.js
@@ -12,7 +12,7 @@ const connectDB = async () => {
 
     console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
-    console.error(`Error: ${error.message}`);
+    console.error(`Error connecting mongoDB: ${error.message} host: ${process.env.DB_HOST}`);
     process.exit(1);
   }
 };
